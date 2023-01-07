60eb58bae9bc809f3a610a78dc5bd748fa82742e
KuanWenChen <dodo890806@gmail.com>
2023-01-07 19:02:26 +0800

    (UPDATE) 調整 prisma

---
+++ src/common/provider/prisma/prisma.service.ts
@@ -0,0 +1,22 @@
+import {
+  Injectable,
+  OnModuleInit,
+  INestApplication,
+  Logger,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('PrismaService');
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Connect to Database succeed!');
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
